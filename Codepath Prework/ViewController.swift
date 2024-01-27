bada33cbb5324bf034a40b397df6f271e1a78ed8
Deborah Mong'are <dkmongare@meredith.edu>
2024-01-26 22:24:24 -0500

    Initial Commit

--- Codepath Prework/ViewController.swift
+++ Codepath Prework/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Improvement to change text color for my name label
+    @IBOutlet weak var myFirstLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor{
+            
 
+                let red = CGFloat.random(in: 0...1)
+                let green = CGFloat.random(in: 0...1)
+                let blue = CGFloat.random(in: 0...1)
+
+                return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            }
+        let randomColor = changeColor()
+        //Color change for the label with my name
+        let newTextColor = changeColor()
+        view.backgroundColor = randomColor
+        myFirstLabel.textColor = newTextColor
+    }
+    
 }
 
